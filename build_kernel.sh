d92e5464729c7df045a2524f5546df99b145f08b
renoxtv <guyguygftyfty@gmail.com>
2020-02-16 17:32:58 +0200

    build with linaro 7.5.0

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,7 +2,7 @@
 
 #Build kernel
 export ARCH=arm64
-export CROSS_COMPILE=~/kernel/toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=~/kernel/toolchain/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 
 export ANDROID_MAJOR_VERSION=q
 
--- dts.sh
+++ dts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build
-export CROSS_COMPILE=~/kernel/toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=~/kernel/toolchain/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 export ARCH=arm64
 export ANDROID_MAJOR_VERSION=q
 export PLATFORM_VERSION=10.0.0
